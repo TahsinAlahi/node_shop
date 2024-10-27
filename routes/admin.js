3b54ea25a0af13b96356695ab84bb27f8094a756
Tahsin <alahitahsin@gmail.com>
2024-10-27 16:20:18 +0600

    changed admin to admin.ejs

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,8 @@ const path = require("path");
 const rootDir = require("../util/rootDir");
 
 router.route("/(.html)?").get((req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "admin.html"));
+  // res.sendFile(path.join(rootDir, "views", "admin.html"));
+  res.render("admin");
 });
 
 router.route("/add-product").post((req, res, next) => {
